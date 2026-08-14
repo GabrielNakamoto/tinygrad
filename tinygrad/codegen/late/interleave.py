a6e1c3f4c902e5828a002406c68d5a65456449e2
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-08-14 09:30:15 -0300

    scale up non-bijective

--- tinygrad/codegen/late/interleave.py
+++ tinygrad/codegen/late/interleave.py
@@ -4,8 +4,9 @@ from tinygrad.uop.ops import AddrSpace, PatternMatcher, UPat
 class WMMASchedulePolicy:
   def __init__(self, sink:UOp):
     lst = list(sink.toposort())
+    self.schedule_past: dict[UOp, tuple[UOp,...]] = {}
 
-    loads_into: dict[UOp, list[UOp]] = {}
+    loads_into: dict[UOp, set[UOp]] = {}
     for u in lst:
       # find loads used by wmma
       if u.op is not Ops.WMMA: continue
@@ -14,27 +15,18 @@ class WMMASchedulePolicy:
         for l in s.src:
           while l.op in {Ops.CAST, Ops.BITCAST}: l=l.src[0]
           if l.op is Ops.LOAD and l.src[0].addrspace is not AddrSpace.REG:
-            loads_into.setdefault(u, []).append(l)
+            loads_into.setdefault(u, set()).add(l)
 
-    # detect overlapping loads? used for multiple WMMAS
-    # ususally multiple wmmas use the same outputs and should be placed in the same block
-    overlaps: dict[UOp, UOp] = {}
+    sched_groups: dict[tuple[UOp,...], list[UOp]] = {}
     for w,ls in loads_into.items():
-      if w in overlaps.values(): continue
-      for ow,ols in loads_into.items():
-        if w is ow: continue
-        if (n := len(set(ls).intersection(set(ols)))) > 0:
-          overlaps[w]=ow
+      sched_groups.setdefault(tuple(ls), []).append(w)
 
-    # for all blocks but the first insert AFTER targeting both previous overlapping WMMAs
+    if len(sched_groups) == 0: return
 
-    # the after needs to go on the loads
-    self.schedule_past: dict[UOp, tuple[UOp,...]] = {}
-    scheds = list(overlaps.items())
-    for i,(a,_) in enumerate(scheds):
-      if i == 0: continue
-      for l in loads_into[a]:
-        self.schedule_past[l] = scheds[i-1]
+    done: list[UOp] = list(sched_groups.values())[0]
+    for ls,ws in list(sched_groups.items())[1:]:
+      for l in ls: self.schedule_past[l] = done
+      done.extend(ws)
 
 pm_schedule_interleave_wmma = PatternMatcher([
   (UPat(Ops.LOAD, name="x"), lambda ctx,x: x.replace(src=(x.src[0].after(*ctx.schedule_past[x]),)+x.src[1:]) if x in ctx.schedule_past else None),
