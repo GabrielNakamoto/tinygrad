0973746b10a69524bafd7853daa750c7271aa23a
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-08-16 10:37:48 -0300

    simplify

--- tinygrad/codegen/late/interleave.py
+++ tinygrad/codegen/late/interleave.py
@@ -10,23 +10,15 @@ class WMMASchedulePolicy:
   def __init__(self, sink:UOp):
     self.schedule_past: dict[UOp, tuple[UOp,...]] = {}
 
-    # wmma -> load deps per operand
     wmma_deps: dict[UOp, list[set[UOp]]] = {}
     wmma_consumers: dict[UOp, list[UOp]] = {}
     # find loads used by wmma
-    def _seek(u:UOp) -> UOp:
-      while u.op in {Ops.CAST, Ops.BITCAST, Ops.INDEX}: u=u.src[0]
-      return u
     for u in sink.toposort():
       if u.op is Ops.WMMA:
         for s in u.src:
-          assert s.op is Ops.STACK
-          deps = set()
-          for l in s.src:
-            l = _seek(l)
-            if l.op is Ops.LOAD and l.src[0].addrspace is not AddrSpace.REG:
-              deps.add(l)
-          wmma_deps.setdefault(u, []).append(set(deps))
+          deps = set(l for l in s.toposort() if l.op is Ops.LOAD and l.src[0].addrspace is not AddrSpace.REG)
+          wmma_deps.setdefault(u, []).append(deps)
+      # dont rely on store? use last src edge to the WMMA?
       if u.op is Ops.STORE:
         if u.src[1].op is Ops.INDEX:
           w = u.src[1].src[0]
@@ -35,26 +27,22 @@ class WMMASchedulePolicy:
     if len(wmma_deps) < 4: return
 
     sched_groups: dict[frozenset[UOp], list[UOp]] = {}
-    wmma_a_deps = {k:v[0] for k,v in wmma_deps.items()}
-    for w,ls in wmma_a_deps.items():
-      # group by shared A fragment
-      best = max((l.intersection(ls) for l in wmma_a_deps.values() if l is not ls), key=lambda lis: len(lis))
-      sched_groups.setdefault(frozenset(best), []).append(w)
+    for w in wmma_deps.keys():
+      sched_groups.setdefault(frozenset(wmma_deps[w][0]), []).append(w)
 
-    groups = list(sched_groups.values())
+    # transitive, only schedule past last block not all preceding
+    carry: list[UOp] = []
     scheduled: set[UOp] = set()
-    wmmas: list[UOp] = []
-    for i,g in enumerate(groups):
+    for i,g in enumerate(sched_groups.values()):
       deps = set()
       for w in g:
         for ls in wmma_deps[w]: deps.update(ls)
       if i > 0:
         for l in deps:
-          if l in scheduled: continue
-          self.schedule_past[l] = tuple(wmmas)
+          if l not in scheduled: self.schedule_past[l] = tuple(carry)
       scheduled.update(deps)
-      wmmas.extend(g)
-      for w in g: wmmas.extend(wmma_consumers[w])
+      carry = list(g)
+      for w in g: carry.extend(wmma_consumers[w])
 
 pm_schedule_interleave_wmma = PatternMatcher([
   (UPat((Ops.LOAD, Ops.STORE), name="x"), lambda ctx,x: x.replace(src=(x.src[0].after(*ctx.schedule_past[x]),)+x.src[1:]) if x in ctx.schedule_past else None),
