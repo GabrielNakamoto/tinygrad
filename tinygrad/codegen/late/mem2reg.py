a2dfbc8bbf7f73b2f95bbeb9bd9104393723dad6
Gabriel Nakamoto <gabriel@nakamoto.ca>
2026-08-21 10:51:42 -0400

    better range context

--- tinygrad/codegen/late/mem2reg.py
+++ tinygrad/codegen/late/mem2reg.py
@@ -11,54 +11,60 @@ def bptr(x:UOp) -> tuple[UOp, int]:
 # promotes REG space BUFFER memory loads/stores to SSA registers through control flow analysis/PHI resolution
 # https://llvm.org/docs/Passes.html#mem2reg-promote-memory-to-register
 class Mem2regContext:
-  def r(self): self.rc += 1
   # in tinygrad phis are only necessary for loop carried dependencies ex.
   # stores that occur between load and one or more backedges
   def __init__(self, lst:list[UOp], ren:Renderer):
     assert isinstance(ren, ISARenderer), "mem2reg only supported for assembly backends"
-    self.ren, self.rc = ren, 0
+    # TODO: simplify all these data structures...
+    self.ren = ren
+    self.current: dict[UOp, UOp] = {}
+    self.nl: dict[tuple[UOp, int], int] = {}
     lane_ctr = itertools.count()
     rng_stack: list[UOp] = []
     current: dict[tuple[UOp, int], UOp] = {}
     rng_head: dict[UOp, dict[tuple[UOp, int], UOp]] = {}
     rng_backedge: dict[UOp, dict[tuple[UOp, int], UOp]] = {}
-    redge: dict[UOp, VRegister] = {}
-    self.phis: dict[tuple[UOp, int], dict[int, UOp]] = {}
-    n = 0
+    flat: dict[UOp, VRegister] = {}
+    self.phis: dict[tuple[tuple[UOp, int], int], UOp] = {}
+    n: dict[tuple[UOp, int], int] = {}
+    nl: dict[UOp, int] = {}
     for u in lst:
       if u.op in {Ops.STORE, Ops.LOAD}:
         buf, i = bptr(u.src[0])
         if buf.addrspace is not AddrSpace.REG: continue
-
         # register first load and last store of each range
         if len(rng_stack):
-          if u.op is Ops.LOAD and (buf,i) not in rng_head.setdefault(rng_stack[-1], {}):
-            rng_head[rng_stack[-1]][(buf,i)]=u
+          rng = rng_stack[-1]
+          if u.op is Ops.LOAD and (buf,i) not in rng_head.setdefault(rng, {}):
+            if rng in rng_backedge and (buf,i) in rng_backedge[rng]: rng_backedge[rng].pop((buf,i))
+            rng_head[rng][(buf,i)]=u
           if u.op is Ops.STORE:
-            rng_backedge.setdefault(rng_stack[-1], {})[(buf,i)] = u
-
-        # otherwise load maps to last stores value?
+            rng_backedge.setdefault(rng, {})[(buf,i)] = u
         if u.op is Ops.STORE: current[(buf,i)] = u
-        if u.op is Ops.LOAD: redge[u] = rdef(current[(buf,i)])
-      if u.op is Ops.RANGE:
-        n += 1
-        rng_stack.append(u)
+        if u.op is Ops.LOAD:
+          n[(buf,i)] = n.setdefault((buf,i), 0) + 1
+          nl[u] = n[(buf,i)]
+          flat[u] = rdef(current[(buf,i)])
+      if u.op is Ops.RANGE: rng_stack.append(u)
       if u.op is Ops.END:
-        rng = rng_stack.pop()
-        if rng in rng_head:
-          for ptr,l in rng_head[rng].items():
+        if (rng := rng_stack.pop()) in rng_head:
+          for ptr,l in rng_head.pop(rng).items():
             if rng in rng_backedge and ptr in rng_backedge[rng]:
               # phi between last flat store and store backedge
-              cur, bedge = redge[l], rdef(rng_backedge[rng][ptr])
-              vr = ren.vreg(cur.cons, width=cur.width, alignment=cur.width, phi=(cur,bedge))
+              cur, bedge = flat[l], rdef(rng_backedge[rng][ptr])
+              vr = ren.vreg(cur.cons, width=cur.width, alignment=cur.alignment, phi=(cur,bedge))
+              print("inserting backedge phi:", ptr[0].arg, ptr[1], f"{vr.phi} => {vr}", nl[l])
               phi = UOp.placeholder((1,), ptr[0].dtype, next(lane_ctr), AddrSpace.REG).replace(tag=(vr,))
-              self.phis.setdefault(ptr, {})[n] = phi
-    self.current: dict[UOp, UOp] = {}
+              self.phis[(ptr, nl[l])] = phi
+
+  def try_phi(self, idx:UOp, x:UOp) -> UOp|None:
+    ptr = bptr(idx)
+    self.nl[ptr] = self.nl.setdefault(ptr, 0) + 1
+    phi = self.phis.get((ptr, self.nl[ptr]), None)
+    return (phi, [phi]) if phi is not None else None
 
 pm_insert_phis = PatternMatcher([
-  (UPat(Ops.RANGE, name="rng"), lambda ctx,rng: ctx.r()),
-  (UPat.var("idx").load(name="x"), lambda ctx,idx,x:
-    ((nx := ctx.phis[bptr(idx)][ctx.rc]), [nx]) if bptr(idx) in ctx.phis else None),
+  (UPat.var("idx").load(name="x"), lambda ctx,idx,x: ctx.try_phi(idx, x)),
 ])
 
 # REG store copy/coalesce is handled by regalloc PHI logic
